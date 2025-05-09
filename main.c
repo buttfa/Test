bd8a36ca709458fa13a7fc36b6182602e4cb35be
buttfa <1662332017@qq.com>
2025-05-09 19:58:43 +0800

    来自张黎的更改

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-int main()
-{
-    printf("Hello World!");
-    printf("Hello World!");
-    printf("Hello World!");
+int main() {
+  printf("Hello World!");
+  printf("Hello World!");
+  printf("Hello World!");
 
-    printf("commit");
+  printf("commit");
 
-    printf("commit from yangxinxin.");
+  printf("commit from yangxinxin.");
 
-    printf("commit from yangxinxin Test.");
+  printf("commit from yangxinxin Test.");
 
-    return 0;
+  printf("zhangli");
+
+  return 0;
 }
